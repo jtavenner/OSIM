2f2bf07aa64938da096ecf48f71e21713ee77bf2
jtavenner <username@users.noreply.github.com>
2018-06-17 14:30:20 -0400

    Completed integration tests that do not work appear to be running.

--- OSIM.IntegrationTests/OSIM.Core.Persistence/ItemTypePersistenceTests.cs
+++ OSIM.IntegrationTests/OSIM.Core.Persistence/ItemTypePersistenceTests.cs
@@ -4,21 +4,13 @@ using Ninject;
 using OSIM.Core.Entities;
 using OSIM.UnitTests;
 using OSIM.Core.Persistence;
-
+using System;
 
 namespace OSIM.IntegrationTests.OSIM.Core.Persistence
 {
     public class when_using_the_item_type_repository : Specification
     {
-
-    }
-
-    public class and_attempting_to_save_and_read_a_value_from_the_datastore : when_using_the_item_type_repository
-    {
-        private ItemType _expected;
-        private ItemType _result;
-
-        protected  ItemTypeRepository _itemTypeRepository;
+        protected ItemTypeRepository _itemTypeRepository;
         protected StandardKernel _kernel;
 
         protected override void Establish_context()
@@ -26,7 +18,19 @@ namespace OSIM.IntegrationTests.OSIM.Core.Persistence
             base.Establish_context();
             _kernel = new StandardKernel(new IntegrationTestModule());
         }
+    }
 
+    public class and_attempting_to_save_and_read_a_value_from_the_datastore : when_using_the_item_type_repository
+    {
+        private ItemType _expected;
+        private ItemType _result;        
+
+        protected override void Establish_context()
+        {
+            base.Establish_context();
+
+            _expected = new ItemType { Name = Guid.NewGuid().ToString() };
+        }
 
         protected override void Because_of()
         {
